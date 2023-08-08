572f9b12ca1d5cb7aa7a7e979566f7c9f9022d0d
jeffreymrobertson <jeffreymrobertson@gmail.com>
2023-08-08 19:40:15 -0400

    V2.0

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 ## Project Description
 Analysis and output of weather readings
 
+
+# Usage
+
+API Routes
+/api/v1.0/precipitation
+/api/v1.0/stations
+/api/v1.0/tobs
+/api/v1.0/(start date format = yyyy-mm-dd)
+/api/v1.0/(start)/(end)
+
 ## Contributors
 Jeffrey Robertson
--- app.py
+++ app.py
@@ -50,53 +50,80 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    #starts the session
     session = Session(engine)
+    #sets the date
     query_date= dt.date(2017, 8 ,23) - dt.timedelta(days=365)
-    query_data = session.query(Measurement.date, func.sum(Measurement.prcp)).group_by(Measurement.date).order_by(Measurement.date).\
-    filter(Measurement.date >= query_date).all()
+    #queries the data
+    query_data = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= query_date).\
+        order_by(Measurement.date).all()
+    precipitation = {date:prcp for date,prcp in query_data}
     session.close()
-    year_rain = []
-    for date, prcp in query_data:
-        total_rain={}
-        total_rain[date]=prcp
-        year_rain.append(total_rain)
-    return jsonify(year_rain)
+    return jsonify(precipitation)
 
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-    results = session.query(Station.station).all()
+    result = []
+    results = session.query(Station.station, Station.id).all()
+    #add the station name and id to a dictionairy and add them to a list
+    for station, id in results:
+        stations_dict = {}
+        stations_dict['station'] = station
+        stations_dict['id'] = id
+        result.append(stations_dict)
     session.close()
-    station = list(np.ravel(results))
-    return jsonify(station)
+    return jsonify(result)
 
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    session = Session(engine)
     query_date= dt.date(2017, 8 ,23) - dt.timedelta(days=365)
-    mvp = session.query(Station.station,Measurement.date, Measurement.tobs).\
-    filter(Station.id == 7 ,Measurement.date >= query_date).all()
+    results = session.query(Measurement.station).\
+                order_by(func.count(Measurement.station).desc()).\
+                group_by(Measurement.station).first()
+    #gets the most active data
+    result_one = results[0]
+    years_data = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.station == result_one).\
+        filter(Measurement.date >= query_date)
     most_active = []
-    for station, date, temp in mvp:
+    for date, temp in years_data:
         md = {}
-        md[date] = temp
+        md['date'] = date
+        md['tobs'] = temp
         most_active.append(md)
         print(Measurement.date)
+    session.close()
     return jsonify(most_active)
 
         
 
 @app.route("/api/v1.0/<start>")
 def tobsave(start): 
-    most_active = session.query(Measurement.tobs).\
-    filter(func.strftime("%Y-%m-%d",Measurement.date)==start).all()
-    low = np.min(most_active)
-    high = np.max(most_active)
-    average = np.mean(most_active)
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+                                filter(Measurement.date == start).all()
     tobs_data = []
-    tobs_dict = {start:{"Low":low,"High": high, "Average": average}}
-    tobs_data.append(tobs_dict)
-    return jsonify(tobs_data)
+    for minimum, maximum, average in results:
+        result_dic = {}
+        result_dic['Minimum Temperature'] = minimum
+        result_dic['Maximum Temperature'] = maximum
+        result_dic['Average Temperature'] = average
+        tobs_data.append(result_dic)
+        #if there is data then return the data
+        if result_dic['Minimum Temperature']:
+            session.close()
+            return jsonify(tobs_data)
+        #if it doesn't have a value return an error message
+        else:
+            session.close()
+            return jsonify('There is no data for this date please select a new date')
+    
+        
+
 
 @app.route("/api/v1.0/<start>/<end>")
 def range(start,end):
@@ -108,6 +135,7 @@ def range(start,end):
         result = {}
         result[date] = {"Low": min, "High": max, "Average": ave}
         ave_temp.append(result)
+    session.close()
     return jsonify(ave_temp)
 if __name__ == '__main__':
     app.run(debug=True)
